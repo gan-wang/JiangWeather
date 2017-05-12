5881504776858a1b981e1055ac9b8e0490f444b7
Gan <767214784@qq.com>
2017-05-12 12:00:44 +0800

    加入数据库和表的各项配置。

---
+++ app/src/main/java/com/jiangweather/android/db/County.java
@@ -0,0 +1,47 @@
+package com.jiangweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by wang on 2017/5/12 0012.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    private int cityId;
+}
